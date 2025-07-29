315839541c51e221c00e99015cb4c4565931bc93
jvoye23 <joerg.voye@gmail.com>
2025-07-29 12:44:45 +0200

    Adding RegisterScreen Adaptive Layouts

--- app/src/main/java/com/jvoye/tasky/auth/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/jvoye/tasky/auth/presentation/register/RegisterScreen.kt
@@ -6,15 +6,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -28,7 +35,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withLink
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -40,6 +49,7 @@ import com.jvoye.tasky.core.presentation.designsystem.theme.Icon_Check
 import com.jvoye.tasky.core.presentation.designsystem.theme.TaskyTheme
 import com.jvoye.tasky.core.presentation.designsystem.theme.link
 import com.jvoye.tasky.core.presentation.designsystem.theme.surfaceHigher
+import com.jvoye.tasky.core.presentation.designsystem.util.DeviceConfiguration
 
 @Composable
 fun RegisterScreenRoot(
@@ -64,47 +74,177 @@ private fun RegisterScreen(
             .fillMaxSize(),
         contentWindowInsets = WindowInsets.statusBars
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            RegistrationHeaderSection()
 
-            val rootModifier = Modifier
-                .fillMaxSize()
-                .clip(
-                    RoundedCornerShape(
-                        topStart = 24.dp,
-                        topEnd = 24.dp
-                    )
-                )
-                .background(MaterialTheme.colorScheme.surface)
-                .padding(
-                    horizontal = 16.dp,
-                    vertical = 28.dp
-                )
+        val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+        val deviceConfiguration = DeviceConfiguration.fromWindowSizeClass(windowSizeClass)
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-
-            ) {
+        when(deviceConfiguration) {
+            DeviceConfiguration.MOBILE_PORTRAIT -> {
                 Column(
-                    modifier = rootModifier
-
+                    modifier = Modifier
+                        .padding(innerPadding)
                 ) {
-                    RegistrationFormSection(
-                        state = state,
-                        onAction = onAction
+                    RegistrationHeaderSection(
+                        modifier = Modifier
+                            .padding(top = 40.dp, bottom = 36.dp),
+                        headerText = stringResource(R.string.create_your_account)
                     )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(
+                                RoundedCornerShape(
+                                    topStart = 24.dp,
+                                    topEnd = 24.dp
+                                )
+                            )
+                            .background(MaterialTheme.colorScheme.surface)
+                            .padding(
+                                horizontal = 16.dp,
+                                vertical = 28.dp
+                            )
+                            .consumeWindowInsets(WindowInsets.navigationBars)
+                    ) {
+                        RegistrationFormSection(
+                            state = state,
+                            onAction = onAction
+                        )
 
-                    Spacer(modifier = Modifier.height(32.dp))
+                        Spacer(modifier = Modifier.height(32.dp))
 
-                    RegistrationButtonSection(
-                        onAction = onAction
-                    ) 
+                        RegistrationButtonSection(
+                            onAction = onAction
+                        )
+                    }
+                }
+            }
+            DeviceConfiguration.MOBILE_LANDSCAPE -> {
+                val bottomOffset: Dp = innerPadding.calculateTopPadding()
+                Row(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(280.dp)
+                            .padding(start = 16.dp, end = 16.dp)
+                            .padding(bottom = bottomOffset),
+                    ) {
+                        RegistrationHeaderSection(
+                            headerText = stringResource(R.string.create_your_account_landscape)
+                        )
+                    }
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(
+                                start = 16.dp,
+                                end = 16.dp
+                            )
+                            .weight(1f)
+                            .clip(
+                                RoundedCornerShape(
+                                    topStart = 24.dp,
+                                    topEnd = 24.dp
+                                )
+                            )
+                            .background(MaterialTheme.colorScheme.surface)
+                            .padding(
+                                vertical = 24.dp,
+                                horizontal = 48.dp
+                            )
+                            .consumeWindowInsets(WindowInsets.navigationBars)
+                    ) {
+                        item {
+                            RegistrationFormSection(
+                                state = state,
+                                onAction = onAction
+                            )
+                        }
+                        item {
+                            Spacer(modifier = Modifier.height(32.dp))
+
+                            RegistrationButtonSection(
+                                onAction = onAction
+                            )
+                        }
+                    }
+                }
+            }
+            DeviceConfiguration.TABLET_PORTRAIT -> {
+                Column(
+                    modifier = Modifier
+                        .padding(innerPadding),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    RegistrationHeaderSection(
+                        modifier = Modifier
+                            .padding(top = 248.dp, bottom = 36.dp),
+                        headerText = stringResource(R.string.create_your_account)
+                    )
+                    Column(
+                        modifier = Modifier
+                            .width(500.dp)
+                            .height(450.dp)
+                            .clip(RoundedCornerShape(24.dp))
+                            .background(MaterialTheme.colorScheme.surface)
+                            .padding(
+                                horizontal = 24.dp,
+                                vertical = 32.dp
+                            )
+                            .consumeWindowInsets(WindowInsets.navigationBars)
+                    ) {
+                        RegistrationFormSection(
+                            state = state,
+                            onAction = onAction
+                        )
+
+                        Spacer(modifier = Modifier.height(32.dp))
+
+                        RegistrationButtonSection(
+                            onAction = onAction
+                        )
+                    }
+                }
+            }
+            DeviceConfiguration.TABLET_LANDSCAPE,
+            DeviceConfiguration.DESKTOP -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    RegistrationHeaderSection(
+                        modifier = Modifier
+                            .padding(bottom = 36.dp),
+                        headerText = stringResource(R.string.create_your_account)
+                    )
+                    Column(
+                        modifier = Modifier
+                            .width(500.dp)
+                            .heightIn(450.dp)
+                            .clip(RoundedCornerShape(24.dp))
+                            .background(MaterialTheme.colorScheme.surface)
+                            .padding(
+                                horizontal = 24.dp,
+                                vertical = 32.dp
+                            )
+                            .consumeWindowInsets(WindowInsets.navigationBars)
+                    ) {
+                        RegistrationFormSection(
+                            state = state,
+                            onAction = onAction
+                        )
+
+                        Spacer(modifier = Modifier.height(32.dp))
+
+                        RegistrationButtonSection(
+                            onAction = onAction
+                        )
+                    }
                 }
             }
         }
@@ -112,17 +252,20 @@ private fun RegisterScreen(
 }
 
 @Composable
-private fun RegistrationHeaderSection() {
+private fun RegistrationHeaderSection(
+    modifier: Modifier = Modifier,
+    headerText: String,
+) {
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 40.dp, bottom = 36.dp),
+        modifier = modifier
+            .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
     ) {
         Text(
-            text = stringResource(R.string.create_your_account),
-            style = MaterialTheme.typography.headlineLarge
+            text = headerText,
+            style = MaterialTheme.typography.headlineLarge,
+            textAlign = TextAlign.Center
         )
     }
 }
@@ -225,7 +368,8 @@ fun RegistrationButtonSection(
     )
 }
 
-@Preview
+@Preview(device = Devices.PIXEL_6_PRO, showSystemUi = true)
+@Preview(device = Devices.PIXEL_TABLET, showSystemUi = true)
 @Composable
 private fun RegisterScreenPreview() {
     TaskyTheme {

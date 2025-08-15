f894ffad4fb62393072f8e5cc1d76a6dc9abadd3
jvoye23 <joerg.voye@gmail.com>
2025-08-16 00:19:30 +0200

    Adding Encrypted Token DataStore

---
+++ app/src/main/java/com/jvoye/tasky/core/data/auth/AuthInfoMapper.kt
@@ -0,0 +1,19 @@
+package com.jvoye.tasky.core.data.auth
+
+import com.jvoye.tasky.core.domain.AuthInfo
+
+fun AuthInfo.toAuthInfoSerializable(): AuthInfoSerializable {
+    return AuthInfoSerializable(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        userId = userId
+    )
+}
+
+fun AuthInfoSerializable.toAuthInfo(): AuthInfo {
+    return AuthInfo(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        userId = userId
+    )
+}
---
+++ app/src/main/java/com/jvoye/tasky/core/data/auth/AuthInfoSerializer.kt
@@ -0,0 +1,39 @@
+package com.jvoye.tasky.core.data.auth
+
+import androidx.datastore.core.Serializer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.Base64
+
+object AuthInfoSerializer: Serializer<AuthInfoSerializable> {
+    override val defaultValue: AuthInfoSerializable
+        get() = AuthInfoSerializable()
+
+    override suspend fun readFrom(input: InputStream): AuthInfoSerializable {
+        val encryptedBytes = withContext(Dispatchers.IO) {
+            input.use { it.readBytes() }
+        }
+        val encryptedBytesDecoded = Base64.getDecoder().decode(encryptedBytes)
+        val decryptedBytes = Crypto.decrypt(encryptedBytesDecoded)
+        val decodedJsonString = decryptedBytes.decodeToString()
+        return Json.decodeFromString(decodedJsonString)
+    }
+
+    override suspend fun writeTo(
+        t: AuthInfoSerializable,
+        output: OutputStream
+    ) {
+        val json = Json.encodeToString(t)
+        val bytes = json.toByteArray()
+        val encryptedBytes = Crypto.encrypt(bytes)
+        val encryptedBytesBase64 = Base64.getEncoder().encode(encryptedBytes)
+        withContext(Dispatchers.IO) {
+            output.use {
+                it.write(encryptedBytesBase64)
+            }
+        }
+    }
+}
